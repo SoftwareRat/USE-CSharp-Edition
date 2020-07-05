d75bc13e418bdfa4e4379e351c0b25b907b41629
SoftwareRat <40393784+SoftwareRat@users.noreply.github.com>
2020-07-05 17:38:51 +0000

    Add files via upload

--- USE-CSharp-Edition/Program.cs
+++ USE-CSharp-Edition/Program.cs
@@ -8,6 +8,8 @@ using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
+using System.Security.Permissions;
+using System.Threading;
 
 namespace USE_CSharp_Edition
 {
@@ -42,23 +44,23 @@ namespace USE_CSharp_Edition
             else
             {
                 Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("NVIDIA GeForce NOW System detected!");
-                Console.ReadKey();
                 Console.ResetColor();
                 Directory.SetCurrentDirectory(@"C:\Program Files (x86)\Steam");
                 WebClient webClient = new WebClient();
                 webClient.DownloadFile("https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US", @"C:\Program Files (x86)\Steam\FirefoxSetup.exe");
-                System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Steam\FirefoxSetup.exe", "/DesktopShortcut=false" + "/InstallDirectoryPath=C:\\Users\\kiosk\\AppData\\Local\\Firefox");
-                Directory.SetCurrentDirectory(@"C:\Users\kiosk\AppData\Local\Firefox");
-                System.IO.File.Move("firefox.exe", "icefox.exe");
-                WebClient Notepad = new WebClient();
+                var p = Process.Start(@"C:\Program Files (x86)\Steam\FirefoxSetup.exe", "/DesktopShortcut=false" + "/InstallDirectoryPath=C:\\Users\\kiosk\\AppData\\Local\\Firefox");
+                p.WaitForExit();
+                System.IO.File.Move(@"C:\Users\kiosk\AppData\Local\Firefox\firefox.exe", @"C:\Users\kiosk\AppData\Local\Firefox\icefox.exe");
+                Directory.SetCurrentDirectory(@"C:\Program Files (x86)\Steam");
                 webClient.DownloadFile("https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.8/npp.7.8.8.bin.x64.7z", @"C:\Users\kiosk\AppData\Local\Notepad++\npp.7.8.8.bin.x64.7z");
-                System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Steam\steamapps\common\Assassins Creed Origins\8za.exe", "x" + "C:\\Users\\kiosk\\AppData\\Local\\Notepad++\\npp.7.8.8.bin.x64.7z");
+                Process.Start(@"C:\Program Files (x86)\Steam\steamapps\common\Assassins Creed Origins\8za.exe", "x" + "C:\\Users\\kiosk\\AppData\\Local\\Notepad++\\npp.7.8.8.bin.x64.7z");
+                p.WaitForExit();
                 Console.BackgroundColor = ConsoleColor.Yellow;
                 Console.ForegroundColor = ConsoleColor.Black;
                 Console.WriteLine("Have fun with testing USE :)");
-                Console.ReadKey();
+                Thread.Sleep(5000);
             }
         }
     }
