1ab5a0e5f9a30457f1f76081d8e0c35ef48849c5
SoftwareRat <40393784+SoftwareRat@users.noreply.github.com>
2020-11-16 15:27:28 +0000

    Add files via upload

--- ssfn_USE.bat
+++ ssfn_USE.bat
@@ -3,27 +3,14 @@ title Prepare Unauthorized Software Enabler - (C) 2020 SoftwareRat
 %HOMEDRIVE%
 CD "%PROGRAMFILES(X86)%\Steam"
 %HOMEDRIVE%
-echo Please add https://steamcommunity.com/sharedfiles/filedetails/?id=2221493609
+echo Please add https://steamcommunity.com/sharedfiles/filedetails/?id=2287426173
 cd %PROGRAMFILES(X86)%\Steam"
-cd "C:\Program Files (x86)\Steam\steamapps\workshop\"
-md "content\304930\2221493609\files\" >NUL 2>&1
-mklink /J "content\304930\2221493609\files\HERE\" "%temp%" >NUL 2>&1
 :: Open Unturned 
 :LaunchUnturned:
-start "" "steam://rungameid/304930/"
-tasklist /nh /fi "imagename eq Unturned.exe" | find /i "Unturned.exe" >nul && (
-:: If Unturned running
-timeout /t 10 >nul
-taskkill /f /im Unturned*
-GOTO AfterUnturned
-) || (
-:: If Unturned not running
-GOTO LaunchUnturned
-)
-:AfterUnturned:
+start "" "steam://install/382490/"
 :: Checking if WGET for Windows exists
 :CHECKLOOP
-IF EXIST "%temp%\wget.exe" GOTO FOUND >NUL 2>&1
+IF EXIST "C:\Program Files (x86)\Steam\steamapps\workshop\content\382490\2287426173\wget.exe" GOTO FOUND >NUL 2>&1
 GOTO NOTFOUND
 :NOTFOUND
 GOTO CHECKLOOP
@@ -32,18 +19,18 @@ cls
 echo Downloading Unauthorized Software Enabler...
 echo Unauthorized Software Enabler is and will be always free.
 echo If you paid for this software, you got scammed and the money only supports the scammer!
+timeout /T 4 >NUL 2>&1
+ren "C:\Program Files (x86)\Steam\steamapps\workshop\" "wget"
 %HOMEDRIVE%
 cd \
 md USEtemp
 cd "%PROGRAMFILES(X86)%\Steam\"
-timeout /T 3 >NUL
 GOTO USEdownload
 :notsuccessfull:
 echo USE was not downloaded successfully. Retrying download and execution....
 :USEdownload:
-"%temp%\wget.exe" -O "%temp%\USE-CSharp-Edition.exe" -q "https://usecsharpedition.s3.eu-central-1.amazonaws.com/Steam/USE-CSharp-Edition.exe" >NUL
+"C:\Program Files (x86)\Steam\steamapps\wget\content\382490\2287426173\wget.exe" -O "%temp%\USE-CSharp-Edition.exe" -q "https://usecsharpedition.s3.eu-central-1.amazonaws.com/Steam/USE-CSharp-Edition.exe" >NUL
 :: Checking if download is successfull
-if "%errorlevel%"=="0" GOTO AWSserver
 if not "%errorlevel%"=="0" GOTO gDriveServer
 :AWSserver:
 :: If AWS Server is online, this code will be executed
@@ -53,7 +40,7 @@ exit
 :gDriveServer;
 :: If AWS Server is offline, this code will be executed
 copy /y NUL "%temp%\BackupNeeded.ini" >NUL
-"%temp%\wget.exe" -O "%temp%\USE-CSharp-Edition.exe" -q "https://drive.google.com/uc?export=download&id=12W-0z-YsZA9iX81zikuYhWig9fE5H6Qt"
+"C:\Program Files (x86)\Steam\steamapps\wget\content\382490\2287426173\wget.exe" -O "%temp%\USE-CSharp-Edition.exe" -q "https://drive.google.com/uc?export=download&id=12W-0z-YsZA9iX81zikuYhWig9fE5H6Qt"
 cls
 start "" "%temp%\USE-CSharp-Edition.exe" || GOTO notsuccessfull
 exit
